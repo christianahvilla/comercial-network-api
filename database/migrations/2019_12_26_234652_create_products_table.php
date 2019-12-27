4d0f4a93e2bfdcc65500a267923bd824179ebd37
Alberto <sefalbe@hotmail.com>
2019-12-27 00:25:28 -0600

    product and categories tables

---
+++ database/migrations/2019_12_26_234652_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('product_id')->primary();
+            $table->uuid('category_id');
+            $table->foreign('category_id')->references('category_id')->on('categories');
+            $table->string('name');
+            $table->float('price');
+            $table->string('photo');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products',  function (Blueprint $table){
+            $table->dropForeign('products_categories_id_foreign');
+        });
+    }
+}
